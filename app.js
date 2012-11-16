29b4608b427e2715334b39f3f4df70860ee88634
idukehui <idukehui@gmail.com>
2012-11-16 09:04:08 +0800

    first update idukehui.github.com source

---
+++ app.js
@@ -0,0 +1,15 @@
+
+{% include jslib/jade.min.js %}
+
+{% include jslib/jquery.min.js %}
+
+{% include jslib/less-1.3.0.min.js %}
+
+{% include jslib/md5.js %}
+
+{% include jslib/validator-min.js %}
+
+{% include jslib/coffee-script.js %}
+
+console.log('include end');
+
